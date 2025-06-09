7ea42d8dbb9349e1ae0dc98841ed798a8a720853
Madie Banks <mb40862@georgiasouthern.edu>
2025-06-09 00:19:55 -0400

    Update Breed List and Model

--- app.py
+++ app.py
@@ -15,7 +15,20 @@ model = tf.keras.models.load_model('breedModel.h5')
 #Load label classes
 label_encoder = LabelEncoder()
 #Actual breed label list
-breed_classes = []
+breed_classes = ["Afghan Hound", "African Wild Dog", "Airedale Terrier", "American Staffordshire Terrier", "Appenzeller Sennenhund", "Australian Terrier", "Bedlington Terrier", 
+                 "Bernese Mountain Dog", "Blenheim Cavalier King Charles Spaniel", "Border Collie", "Border Terrier", "Boston Terrier", "Bouvier Des Flandres", "Brussels Griffon", 
+                 "Brittany", "Cardigan Welsh Corgi", "Chesapeake Bay Retriever", "Chihuahua", "Dandie Dinmont Terrier", "Doberman Pinscher", "English Setter", "English Springer Spaniel", 
+                 "Entlebucher Mountain Dog", "American Eskimo Dog", "German Shepherd", "German Shorthaired Pointer", "Gordon Setter", "Great Dane", "Great Pyrenees", 
+                 "Greater Swiss Mountain Dog", "Ibizan Hound", "Irish Setter", "Irish Terrier", "Irish Water Spaniel", "Irish Wolfhound", "Italian Greyhound", "Japanese Chin", 
+                 "Kerry Blue Terrier", "Labrador Retriever", "Lakeland Terrier", "Leonberger", "Lhasa Apso", "Maltese", "Xoloitzcuintli", "Newfoundland", 
+                 "Norfolk Terrier", "Norwegian Elkhound", "Norwich Terrier", "Old English Sheepdog", "Pekingese", "Pembroke Welsh Corgi", "Pomeranian", "Rhodesian Ridgeback", 
+                 "Rottweiler", "Saint Bernard", "Saluki", "Samoyed", "Scottish Terrier", "Scottish Deerhound", "Sealyham Terrier", "Shih Tzu", "Siberian Husky", 
+                 "Staffordshire Bull Terrier", "Sussex Spanel", "Tibetan Terrier", "Treeing Walker Coonhound", "Weimaraner", "Welsh Springer Spaniel", "West Highland White Terrier", 
+                 "Yorkshire Terrier", "Affenpinscher", "Basenji", "Basset Hound", "Beagle", "Black and Tan Coonhound", "Bloodhound", "Bluetick Coonhound", "Borzoi", "Boxer", "Briard", 
+                 "Bullmastiff", "Cairn Terrier", "Chow Chow", "Clumber Spaniel", "Cocker Spaniel", "Collie", "Curly-Coated Retriever", "Dhole", "Carolina Dog", "Flat-Coated Retriever", 
+                 "Giant Schnauzer", "Golden Retriever", "Belgian Sheepdog", "Keeshond", "Australian Kelpie", "Komondor", "Kuvasz", "Alaskan Malamute", "Belgian Malinois", "Miniature Pinshcer", 
+                 "Poodle (Miniature)", "Miniature Schnauzer", "Papillon", "Pug", "Redbone Coonhound", "Schipperke", "Silky Terrier", "Soft Coated Wheaten Terrier", 
+                 "Poodle (Standard)", "Standard Schnauzer", "Poodle (Toy)", "Toy Fox Terrier", "Vizsla", "Whippet", "Wire Fox Terrier"]
 label_encoder.fit(breed_classes)
 
 #Image preprocessing function
